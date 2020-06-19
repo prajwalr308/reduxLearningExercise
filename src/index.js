9bda31c2991e6c5194036355a756d6ca3d7e502c
crypt0-1 <prajwal>
2020-06-19 15:57:37 +0530

    redux learning exercise link:https://youtu.be/CVpUuw9XSjY

--- src/index.js
+++ src/index.js
@@ -1,14 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { createStore} from "redux";
+import allReducer from './reducers';
+import {Provider} from 'react-redux';
 
+
+
+const store = createStore(allReducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+// //action
+// const increment = () => {
+//   return {
+//     type: "INCREMENT",
+//   };
+// };
+
+// const decrement = () => {
+//   return {
+//     type: "DECREMENT",
+//   };
+// };
+
+// //reducer
+// const counter = (state = 0, action) => {
+//   switch (action.type) {
+//     case "INCREMENT":
+//       return state + 1;
+//     case "DECREMENT":
+//       return state - 1;
+//   }
+// };
+// //create store
+// let store = createStore(counter);
+
+// store.subscribe(() => console.log(store.getState()));
+
+// //dipatch
+// store.dispatch(increment());
+// store.dispatch(increment());
+// store.dispatch(decrement());
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}><App /></Provider>
+    
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
